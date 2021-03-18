Fix the bug in the LCL resistor losses calculation of the BESS-cycle-efficiency system 1

BESS-cycle-efficiency/BESS_System 1/power_losses_calculation.py
@@ -20,6 +20,7 @@ pswitches_inv_sw = np.array(
     scipy.io.loadmat("Pchaves_inv_sw.mat").get("Pchaves_inv_sw")
 )
 i_cap = np.array(scipy.io.loadmat("I_cap.mat").get("I_cap"))
+pbat = np.array(scipy.io.loadmat("Pot_bat.mat").get("Pot_bat"))
 
 # Perdas totais nos capacitores do dc-link em relação ao SOC
 print("Dc-link loss calculation...")
@@ -63,63 +64,69 @@ for i in range(0, len(i_cap)):
 
 plosses_dc_link = np.array(plosses_calc)
 
-# %%
-# # Cálculo das perdas magnéticas nos indutores do filtro LCL
-# print("Core loss calculation: inverter side inductor of the LCL filter...")
+# Cálculo das perdas magnéticas nos indutores do filtro LCL
+print("Core loss calculation: inverter side inductor of the LCL filter...")
 
 
-# def core_loss_func(mag_flux_dens, count):
-#     mag_flux_dens = matlab.double(list(np.append(mag_flux_dens, mag_flux_dens[0])))
-#     time = matlab.double(list(np.arange(0, 1 / param.fn, param.ts)))
-#     count += 1
-#     print(count, end=" ")
-#     return eng.coreloss(
-#         time, mag_flux_dens, float(param.cn), float(param.xn), float(param.kn), 1
-#     )  # perdas em W/m3
+def core_loss_func(mag_flux_dens, count):
+    mag_flux_dens = matlab.double(list(np.append(mag_flux_dens, mag_flux_dens[0])))
+    time = matlab.double(list(np.arange(0, 1 / param.fn, param.ts)))
+    count += 1
+    print(count, end=" ")
+    return eng.coreloss(
+        time, mag_flux_dens, float(param.cn), float(param.xn), float(param.kn), 1
+    )  # perdas em W/m3
 
 
-# bg_ac = np.array(
-#     [
-#         binv[i][int(len(binv[i]) - ((1 / 60) / (1 / (12000 * 120))) + 1) :]
-#         for i in range(0, len(binv))
-#     ]
-# )
-# core_loss = np.array([core_loss_func(bg_ac[i][:], i) for i in range(0, len(binv))])
-# plosses_core_linv_lcl = 3 * np.reshape(core_loss * param.vn * 1e-9, (10, 10))  # Perdas em W
+bg_ac = np.array(
+    [
+        binv[i][int(len(binv[i]) - ((1 / 60) / (1 / (12000 * 120))) + 1) :]
+        for i in range(0, len(binv))
+    ]
+)
 
-# print("\nCore loss calculation: grid side inductor of the LCL filter...")
-# bg_ac = np.array(
-#     [
-#         bg[i][int(len(bg[i]) - ((1 / 60) / (1 / (12000 * 120))) + 1) :]
-#         for i in range(0, len(bg))
-#     ]
-# )
-# core_loss = np.array([core_loss_func(bg_ac[i][:], i) for i in range(0, len(binv))])
-# plosses_core_lg_lcl = 3 * np.reshape(core_loss * param.vn * 1e-9, (10, 10))  # Perdas em W
+core_loss = np.array([core_loss_func(bg_ac[i][:], i) for i in range(0, len(binv))])
+plosses_core_linv_lcl = 3 * core_loss * param.vn * 1e-9  # Perdas em W
 
-# print("\nCopper loss calculation: resistors of the LCL filter...")
-# plosses_copper_lcl = pcp_ind_lcl
+print("\nCore loss calculation: grid side inductor of the LCL filter...")
+bg_ac = np.array(
+    [
+        bg[i][int(len(bg[i]) - ((1 / 60) / (1 / (12000 * 120))) + 1) :]
+        for i in range(0, len(bg))
+    ]
+)
+core_loss = np.array([core_loss_func(bg_ac[i][:], i) for i in range(0, len(binv))])
+plosses_core_lg_lcl = 3 * core_loss * param.vn * 1e-9  # Perdas em W
 
-# print("ESR loss calculation: capacitors of the LCL filter...")
-# plosses_esr_lcl = pcp_ind_lcl
+print("\nCopper loss calculation: resistors of the LCL filter...")
+plosses_copper_lcl = pcp_ind_lcl
 
-# print("Conduction loss calculation: Inverter switches...")
-# plosses_cond_inv = pswitches_inv_cond
+print("ESR loss calculation: capacitors of the LCL filter...")
+plosses_esr_lcl = pcp_ind_lcl
 
-# print("Switching loss calculation: Inverter switches...")
-# plosses_switch_inv = pswitches_inv_sw
+print("Conduction loss calculation: Inverter switches...")
+plosses_cond_inv = pswitches_inv_cond
 
-# print("Total power losses calculation...")
-# total_power_losses = (
-#     plosses_dc_link
-#     + plosses_core_linv_lcl
-#     + plosses_core_lg_lcl
-#     + plosses_copper_lcl
-#     + plosses_esr_lcl
-#     + plosses_cond_inv
-#     + plosses_switch_inv
-# )
+print("Switching loss calculation: Inverter switches...")
+plosses_switch_inv = pswitches_inv_sw
+
+print("Total power losses calculation...")
+total_power_losses = (
+    plosses_dc_link
+    + plosses_core_linv_lcl
+    + plosses_core_lg_lcl
+    + plosses_copper_lcl
+    + plosses_esr_lcl
+    + plosses_cond_inv
+    + plosses_switch_inv
+)
+
+print("Efficiency calculation...")
+efficiency = (1 - total_power_losses / pbat) * 100
+print(efficiency)
 
 # with open("total_losses.json", "w") as arquivo:
 #     total_power_losses_list = total_power_losses.tolist()
 #     json.dump(total_power_losses_list, arquivo)
+
+# %%