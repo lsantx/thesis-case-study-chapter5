8a8b09961df836dc3c6168c3b59a5cd7f4e318d3
Lucas Xavier <lsantx@gmail.com>
2021-03-20 23:07:03 -0300

    Include the files in BESS-Efficiency-map/BESS_System 3/. Fix the rated active power in BESS-Efficiency-map for 100kW. Fix the bug of Pref in BESS-Efficiency-map/BESS_System 2/BESS_system2.plecs.

---
+++ BESS-Efficiency-map/BESS_System 3/efficiency_map.py
@@ -0,0 +1,45 @@
+# %%
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import scipy.io
+
+pbat = np.array(scipy.io.loadmat("Pot_bat.mat").get("Pot_bat"))
+
+with open("total_losses.json", "r") as arquivo:
+    total_power_losses = np.array(json.load(arquivo))
+
+print(pbat)
+efficiency = (1 - total_power_losses / pbat) * 100
+
+pnom = 105e3
+pref = np.array(
+    [
+        pnom,
+        pnom * 0.9,
+        pnom * 0.8,
+        pnom * 0.7,
+        pnom * 0.6,
+        pnom * 0.5,
+        pnom * 0.4,
+        pnom * 0.3,
+        pnom * 0.2,
+        pnom * 0.1,
+    ]
+)
+# pref = np.reshape(pref, (-1, 1))
+print(efficiency)
+soc = np.array([100, 90, 80, 75, 70, 60, 50, 40, 30, 20])
+
+fig, ax1 = plt.subplots(1, 1)
+fig.set_size_inches(8, 6)
+
+
+count1 = ax1.contourf(soc, pref / 1000, efficiency)
+ax1.set_xlabel("Soc [%]")
+ax1.set_ylabel("Power [kW]")
+cbar3 = fig.colorbar(count1, ax=ax1)
+fig.tight_layout()
+plt.show()
+
+# %%
