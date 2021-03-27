6d8039fc538a20acccc77cd735fb2fc20945f01d
Lucas Xavier <lsantx@gmail.com>
2021-03-27 19:27:55 -0300

    Include the file Discharge/BESS-cycle-efficiency/plot_efficiency_cycle.py

--- Discharge/BESS-cycle-efficiency/plot_efficiency_cycle.py
+++ Discharge/BESS-cycle-efficiency/plot_efficiency_cycle.py
@@ -2,7 +2,6 @@
 import numpy as np
 import json
 import matplotlib.pyplot as plt
-import scipy.io
 
 with open("BESS_System 1/efficiency_bess1.json", "r") as arquivo:
     efficiency_bess1 = np.array(json.load(arquivo))
@@ -15,4 +14,23 @@ with open("BESS_System 3/efficiency_bess3.json", "r") as arquivo:
 
 with open("BESS_System 4/efficiency_bess4.json", "r") as arquivo:
     efficiency_bess4 = np.array(json.load(arquivo))
+
+soc = np.array([100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20])
+
+fig, ax = plt.subplots()
+
+ax.plot(soc, efficiency_bess1, 'ko-', label='T1')
+ax.plot(soc, efficiency_bess2, 'ro-', label='T2')
+ax.plot(soc, efficiency_bess3, 'go-', label='T3')
+ax.plot(soc, efficiency_bess4, 'bo-', label='T4')
+ax.set_xlabel('SOC [%]', fontsize=14)
+ax.set_ylabel('Efficiency [%]', fontsize=14)
+ax.set_ylim([91.5, 94])
+plt.yticks(fontsize=12)
+plt.xticks(fontsize=12)
+plt.legend(bbox_to_anchor=[0.2, 0.5], loc='center', fontsize=13)
+plt.grid()
+plt.show(block=False)
+input("hit[enter] to end.")
+plt.close('all')
 # %%
