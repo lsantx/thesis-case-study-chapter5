b28c22a16d44b35c8a9800fa36f47149d9743fc8
Lucas Xavier <lsantx@gmail.com>
2021-05-05 01:08:23 -0300

    Fix the scale in the efficiency map for charge mode

--- Charge/BESS-Efficiency-map/BESS_System 4/efficiency_map.py
+++ Charge/BESS-Efficiency-map/BESS_System 4/efficiency_map.py
@@ -32,9 +32,10 @@ soc = np.array([20, 30, 40, 50, 60, 70, 80, 90, 100])
 fig, ax1 = plt.subplots(1, 1)
 fig.set_size_inches(8, 6)
 
+efficiency = np.round(efficiency, decimals=2)
+
 N = 1000  # Number of levels
-step = (np.amax(efficiency) - np.amin(efficiency)) / N
-levels = np.linspace(np.amin(efficiency), np.amax(efficiency), num=N, endpoint=True)
+levels = np.unique(np.round(np.linspace(87.5, 94.215, num=N, endpoint=True), decimals=2))
 count1 = ax1.contourf(soc, pref / pnom, efficiency, levels, extend="min", cmap="jet")
 ax1.set_xlabel("Soc [%]", fontsize=18)
 ax1.set_ylabel("Power [pu]", fontsize=18)
